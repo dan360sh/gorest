72a6fff2d24d737b06a00430445a4d93887b3b88
Даниил Шитов <dshitov@bivgroup.com>
2022-04-25 17:29:17 +0300

    комит 2

--- cmd/main/app.go
+++ cmd/main/app.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net"
 	"net/http"
-	"time"
 
+	"github.com/gorilla/mux"
 	"github.com/julienschmidt/httprouter"
 )
 
@@ -14,19 +12,29 @@ func IndexHandler(w http.ResponseWriter, r *http.Request, params httprouter.Para
 	name := params.ByName("name")
 	w.Write([]byte(fmt.Sprintf("Hello %s", name)))
 }
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Привет. Это главная страница")
+}
 func main() {
+	router := mux.NewRouter()
+	s := http.StripPrefix("/lol", http.FileServer(http.Dir("./swapi/")))
+	//router.HandleFunc("/", Index)
+	router.PathPrefix("/lol").Handler(s)
+	http.Handle("/", router)
+	http.ListenAndServe(":5000", nil)
+
 	fmt.Println("xxx")
-	router := httprouter.New()
-	router.GET("/:name", IndexHandler)
-	listener, err := net.Listen("tcp", "127.0.0.1:1234")
-	if err != nil {
-		panic(err)
-	}
-	sertver := &http.Server{
-		Handler:      router,
-		WriteTimeout: 15 * time.Second,
-		ReadTimeout:  15 * time.Second,
-	}
-	log.Fatalln(sertver.Serve(listener))
+	// router := httprouter.New()
+	// router.GET("/:name", IndexHandler)
+	// listener, err := net.Listen("tcp", "127.0.0.1:1234")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// sertver := &http.Server{
+	// 	Handler:      router,
+	// 	WriteTimeout: 15 * time.Second,
+	// 	ReadTimeout:  15 * time.Second,
+	// }
+	// log.Fatalln(sertver.Serve(listener))
 
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/jdan360sh/httprouter
 
 go 1.18
 
-require github.com/julienschmidt/httprouter v1.3.0 // indirect
+require (
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/julienschmidt/httprouter v1.3.0 // indirect
+)
